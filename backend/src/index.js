09334048e347b1f4325701a58f5f79e03eef53a2
meassya <assya.asbabou@gmail.com>
2026-02-10 18:42:28 +0100

    index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,33 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
+dotenv.config({ path: '../.env' });
 
-import objectRoutes from "./routes/object.routes.js";
+
+import mongoose from "mongoose";
+import connectDB from "./config/database.js";
+
+/*import objectRoutes from "./routes/object.routes.js";
 import actionsRoutes from "./routes/actions.routes.js";
 import notificationsRoutes from "./routes/notifications.routes.js";
 import subscriptionsRoutes from "./routes/subscriptions.routes.js";
-
-dotenv.config();
+*/
+console.log("MONGO_URI:", process.env.MONGO_URI);
 
 const app = express();
 app.use(express.json());
 
-// ROUTES
-app.use("/api/object", objectRoutes);
-app.use("/api/actions", actionsRoutes);
-app.use("/api/notifications", notificationsRoutes);
-app.use("/api/subscriptions", subscriptionsRoutes);
-
-// TEST ROUTE
-app.get("/api/health", (req, res) => {
-  res.json({ status: "OK", message: "Smart Campus API running" });
-});
 
 // DB
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
+connectDB();
 
 // SERVER
 const PORT = process.env.PORT || 5000;
